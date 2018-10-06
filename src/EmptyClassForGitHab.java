fca40728175dc035acb99abf4b61ae858f5129ff
kostpik <38260652+kostpik@users.noreply.github.com>
2018-10-06 20:13:18 +0300

    commit free

--- src/EmptyClassForGitHab.java
+++ src/EmptyClassForGitHab.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 
 public class EmptyClassForGitHab {
 public void showInGitHub() {
 	System.out.println("this is line");
 	//next step for you
 	//next step two
+Scanner scanner= new Scanner("");
+String reString = scanner.next();
+System.out.println(reString);
 }
+
 }

7f72f9fb72507b2dc173aaa14d5ec1607edc8b7e
kostpik <38260652+kostpik@users.noreply.github.com>
2018-10-06 20:14:42 +0300

    Revert "commit free"
    This reverts commit fca40728175dc035acb99abf4b61ae858f5129ff.

--- src/EmptyClassForGitHab.java
+++ src/EmptyClassForGitHab.java
@@ -1,13 +1,8 @@
-import java.util.Scanner;
 
 public class EmptyClassForGitHab {
 public void showInGitHub() {
 	System.out.println("this is line");
 	//next step for you
 	//next step two
-Scanner scanner= new Scanner("");
-String reString = scanner.next();
-System.out.println(reString);
 }
-
 }
